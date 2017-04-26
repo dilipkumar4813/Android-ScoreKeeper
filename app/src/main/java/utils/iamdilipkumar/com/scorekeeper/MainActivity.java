e8137de1526097d9b1ed44657e2fdde83d00dd46
Dilip <dilip.programmer@gmail.com>
2017-04-26 11:29:31 +0530

    Butterknife and change in ids for textviews

--- app/src/main/java/utils/iamdilipkumar/com/scorekeeper/MainActivity.java
+++ app/src/main/java/utils/iamdilipkumar/com/scorekeeper/MainActivity.java
@@ -2,8 +2,42 @@ package utils.iamdilipkumar.com.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_team_a)
+    TextView mTeamA;
+
+    @BindView(R.id.tv_team_b)
+    TextView mTeamB;
+
+    @BindView(R.id.btn_a_one)
+    Button mAOne;
+
+    @BindView(R.id.btn_a_two)
+    Button mATwo;
+
+    @BindView(R.id.btn_a_three)
+    Button mAThree;
+
+    @BindView(R.id.btn_a_four)
+    Button mAFour;
+
+    @BindView(R.id.btn_b_one)
+    Button mBOne;
+
+    @BindView(R.id.btn_b_two)
+    Button mBTwo;
+
+    @BindView(R.id.btn_b_three)
+    Button mBThree;
+
+    @BindView(R.id.btn_b_four)
+    Button mBFour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
